1183b1464a692af1e9f163892033aab1caea1772
Martin Nordlund <martin@mnordlund.se>
2022-01-04 16:05:00 +0100

    Added status and lock functionality

--- Android/src/GarageController/app/src/main/java/se/mnordlund/garagecontroller/DoorHandler.kt
+++ Android/src/GarageController/app/src/main/java/se/mnordlund/garagecontroller/DoorHandler.kt
@@ -16,4 +16,16 @@ class DoorHandler(private var main: MainActivity, private var appContext: Contex
         Toast.makeText(appContext, "Operating door", Toast.LENGTH_SHORT).show()
     }
 
+    fun lock(lock: Boolean) {
+        val cmds = FirebaseDatabase.getInstance(BuildConfig.DB_URL).getReference("commands")
+        cmds.child("lock").setValue(lock)
+        var message = if(lock) {
+            "Låser"
+        } else {
+            "Låser upp"
+        }
+
+        Toast.makeText(appContext, message, Toast.LENGTH_SHORT).show()
+    }
+
 }
--- Android/src/GarageController/app/src/main/java/se/mnordlund/garagecontroller/MainActivity.kt
+++ Android/src/GarageController/app/src/main/java/se/mnordlund/garagecontroller/MainActivity.kt
@@ -6,7 +6,9 @@ import android.location.Location
 import android.location.LocationRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Switch
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
@@ -17,6 +19,10 @@ import kotlin.math.acos
 import kotlin.math.cos
 import kotlin.math.sin
 import com.google.android.gms.location.LocationResult
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import java.text.DecimalFormat
 
 
@@ -24,6 +30,10 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var doorHandler: DoorHandler
+    private lateinit var isDoorOpenSwitch: Switch
+    private lateinit var isDoorClosedSwitch: Switch
+    private lateinit var isCarInGarage: Switch
+    private lateinit var isLocked: Switch
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,61 +43,41 @@ class MainActivity : AppCompatActivity() {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         doorHandler = DoorHandler(this, applicationContext)
 
+        isDoorOpenSwitch = findViewById<Switch>(R.id.Opened)
+        isDoorClosedSwitch = findViewById<Switch>(R.id.Closed)
+        isCarInGarage = findViewById<Switch>(R.id.CarInGarage)
+        isLocked = findViewById<Switch>(R.id.lock)
+
+        setupListeners()
+
+
+
     }
 
-    fun onTestButton(view: View) {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-                ActivityCompat.requestPermissions(this@MainActivity,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
-            } else {
-                ActivityCompat.requestPermissions(this@MainActivity,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+    fun setupListeners() {
+        var status = FirebaseDatabase.getInstance(BuildConfig.DB_URL).getReference("status")
+
+        val statusListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                isDoorOpenSwitch.isChecked = snapshot.child("doorOpened").value as Boolean
+                isDoorClosedSwitch.isChecked = snapshot.child("doorClosed").value as Boolean
+                isCarInGarage.isChecked = snapshot.child("carInGarage").value as Boolean
+                isLocked.isChecked = snapshot.child("locked").value as Boolean
             }
-            return
-        }
 
-        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location?  ->
-            if(location != null) {
-                var garage = Location("")
-                garage.latitude = BuildConfig.LAT
-                garage.longitude = BuildConfig.LONG
-                var door = Location("")
-                door.latitude = 56.906803
-                door.longitude = 12.509708
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(
+                    applicationContext,
+                    "Failed to read status",
+                    Toast.LENGTH_SHORT
+                ).show()
 
-                var distance = location.distanceTo(garage)
-                var builder = AlertDialog.Builder(this)
-
-                builder.setTitle("Location")
-                builder.setMessage(
-                    location.toString() + garage.toString() + "Distance: " + distance
-                )
-                builder.setPositiveButton(android.R.string.ok) { _, _ ->
-                    Toast.makeText(
-                        applicationContext,
-                        "I'll do it",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-                builder.setNegativeButton(android.R.string.cancel) { _, _ ->
-                    Toast.makeText(
-                        applicationContext,
-                        "Nehepp!",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-                builder.show()
+                Log.w("gc", "load status cancelled", error.toException())
             }
         }
+
+        status.addValueEventListener(statusListener)
+
     }
 
     fun checkPermissionForLocation(): Boolean{
@@ -142,7 +132,7 @@ class MainActivity : AppCompatActivity() {
                 var distance =location.distanceTo(garage)
 
                 // If we are too far away confirm opening the door
-                if(distance < 10) {
+                if(distance < 20) {
                     doorHandler.operateDoor()
                 }else {
                     var builder = AlertDialog.Builder(this)
@@ -167,7 +157,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun onLock(view: View) {
+        doorHandler.lock(isLocked.isChecked)
+    }
+
     fun onDoIt(view: View) {
-        checkDistanceFromGarage()
+        if(!isLocked.isChecked) {
+            checkDistanceFromGarage()
+        } else {
+            Toast.makeText(applicationContext, "Dörren är låst", Toast.LENGTH_SHORT).show()
+        }
     }
 }
