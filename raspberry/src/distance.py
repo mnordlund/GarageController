aff82d284e68aef1ec11b2295078d862bff85df3
Martin Nordlund <martin@mnordlund.se>
2022-01-31 15:36:59 +0100

    Using median of three values for distance

--- raspberry/src/distance.py
+++ raspberry/src/distance.py
@@ -7,13 +7,7 @@ log = logging.getLogger("gc.dist")
 
 distance: float = 0.0 # Distance to object in cm
 
-def updateDistance():
-    """ 
-    Called every cycle to update the distance 
-
-    Code adapted from: https://tutorials-raspberrypi.com/raspberry-pi-ultrasonic-sensor-hc-sr04/
-    """
-    global distance
+def readDistance() -> float:
     GPIO.output(board.TRIGGERPIN, True)
     time.sleep(0.00001)
     GPIO.output(board.TRIGGERPIN, False)
@@ -26,19 +20,45 @@ def updateDistance():
     while GPIO.input(board.ECHOPIN) == 0:
         if (time.time() - BreakTime) > 0.03:
             log.warning("Failed to get distance")
-            return
+            return 0.03
         StartTime = time.time()
 
     BreakTime = time.time()
     while GPIO.input(board.ECHOPIN) == 1:
         if (time.time() - BreakTime) > 0.03:
             log.warning("Failed to get distance")
-            return
+            return 0.03
         StopTime = time.time()
 
-    TimeElapsed = StopTime - StartTime
+    return StopTime - StartTime
 
-    distance = (TimeElapsed * 34300) / 2
+def median(a, b, c):
+    """
+    Adapted from:
+    https://stackoverflow.com/questions/17158667/minimum-no-of-comparisons-to-find-median-of-3-numbers
+    """
+    x = a - b
+    y = b - c
+    z = a - c
+
+    if x * y > 0:
+        return b
+    if x * z > 0:
+        return c
+    
+    return a
+
+def updateDistance():
+    echos = []
+    for x in range(3):
+        echos.append(readDistance())
+        time.sleep(0.03)
+
+    medianEcho = median(echos[0], echos[1], echos[2])
+
+    if medianEcho < 0.03:
+        distance = (medianEcho * 34300) / 2
+        log.debug("Distance: {:.2f}".format(distance))
 
 def getDistance() -> float:
     """ Returns the current distance """
